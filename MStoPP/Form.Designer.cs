4f5a04c27be9e88d591643017140e33448cb303c
rankineugeneallen <rankineugeneallen@gmail.com>
2020-02-18 21:49:29 -0600

    Initial Commit

---
+++ MStoPP/Form.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MStoPP
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.impTxtBtn = new System.Windows.Forms.Button();
+            this.impTextBox = new System.Windows.Forms.TextBox();
+            this.expBtn = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.dynStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // impTxtBtn
+            // 
+            this.impTxtBtn.Location = new System.Drawing.Point(529, 31);
+            this.impTxtBtn.Name = "impTxtBtn";
+            this.impTxtBtn.Size = new System.Drawing.Size(97, 30);
+            this.impTxtBtn.TabIndex = 0;
+            this.impTxtBtn.Text = "Import TXT";
+            this.impTxtBtn.UseVisualStyleBackColor = true;
+            this.impTxtBtn.Click += new System.EventHandler(this.impTxtBtn_Click);
+            // 
+            // impTextBox
+            // 
+            this.impTextBox.Location = new System.Drawing.Point(12, 35);
+            this.impTextBox.Name = "impTextBox";
+            this.impTextBox.Size = new System.Drawing.Size(511, 22);
+            this.impTextBox.TabIndex = 1;
+            this.impTextBox.TextChanged += new System.EventHandler(this.impTextBox_TextChanged);
+            // 
+            // expBtn
+            // 
+            this.expBtn.Enabled = false;
+            this.expBtn.Location = new System.Drawing.Point(529, 79);
+            this.expBtn.Name = "expBtn";
+            this.expBtn.Size = new System.Drawing.Size(97, 40);
+            this.expBtn.TabIndex = 2;
+            this.expBtn.Text = "Export";
+            this.expBtn.UseVisualStyleBackColor = true;
+            this.expBtn.Click += new System.EventHandler(this.expBtn_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dynStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 136);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(638, 22);
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "Status";
+            // 
+            // dynStatusLabel
+            // 
+            this.dynStatusLabel.Name = "dynStatusLabel";
+            this.dynStatusLabel.Size = new System.Drawing.Size(0, 16);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 158);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.expBtn);
+            this.Controls.Add(this.impTextBox);
+            this.Controls.Add(this.impTxtBtn);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "MediaShout to ProPresenter Converter";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button impTxtBtn;
+        private System.Windows.Forms.TextBox impTextBox;
+        private System.Windows.Forms.Button expBtn;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel dynStatusLabel;
+    }
+}
+
---
+++ MStoPP/Form.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MStoPP
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string[] lines;
+        string path;
+        string[] copyright = new string[3];
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void impTxtBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            fileDialog.RestoreDirectory = true;
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = fileDialog.FileName;
+                impTextBox.Text = path;
+
+                this.lines = File.ReadAllLines(path);
+
+            }
+        }
+
+        private void expBtn_Click(object sender, EventArgs e)
+        {
+            string chunk = "";
+            bool copySet = false;
+            ArrayList newLines = new ArrayList();
+            copySet = false;
+            try
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Console.WriteLine("Got Line: " + lines[i]);
+
+                    //skip the things I know we don't need.
+                    if ((lines[i].Contains("Cue:") && lines[i].Contains("Page:")))
+                        continue;
+
+                    getChunk(i);
+                    i++; //skip -----
+
+
+
+                    //if ((lines[i].Contains("Cue:") && lines[i].Contains("Page:")) ||
+                    //    lines[i].Contains("-----"))
+                    //    continue;
+
+
+                        //if (copySet)
+                        //{
+                        //    if( lines[i+1] == copyright[0] &&
+                        //        lines[i+2] == copyright[1] &&
+                        //        lines[i+3] == copyright[2])
+                        //    {
+                        //        i += 4;
+                        //        newLines.Add("\n");
+                        //        continue;
+                        //    }
+                        //}
+
+                        //if(lines[i] == "" && lines[i + 1] != "")
+                        //{
+                        //    if (!copySet)
+                        //    {
+                        //        copyright[0] = lines[i + 1];
+                        //        copyright[1] = lines[i + 2];
+                        //        copyright[2] = lines[i + 3];
+                        //        i += 3;
+
+                        //        Console.WriteLine("Copyright: \n" +
+                        //            copyright[0] + "\n" +
+                        //            copyright[1] + "\n" +
+                        //            copyright[2] + "\n");
+
+                        //        copySet = true;
+                        //        continue;
+                        //    }
+                        //}   
+
+                        newLines.Add(lines[i]+"\n");
+                    Console.WriteLine("ADDED LINE: " + lines[i] + "\n");
+                }
+
+                writeToFile((string[])newLines.ToArray(typeof(string)));
+    
+                dynStatusLabel.Text = "Done";
+                
+            }
+            catch (IndexOutOfRangeException)
+            {
+                //do nothing, just end cuz we reached the end of the file. 
+                dynStatusLabel.Text = "Done";
+            }
+            finally
+            {
+                writeToFile((string[])newLines.ToArray(typeof(string)));
+
+            }
+
+
+        }
+
+        private string getChunk(int start)
+        {
+            StringBuilder retStr = new StringBuilder();
+            retStr.Clear();
+            for(int i = start; i < lines.Length; i++)
+            {
+                if (lines[i] == "-----")
+                {
+                    break;
+                }
+                if(lines[i] == "" && !copyrightLoaded()) //check for copyright
+                {
+                    loadCopyright(i);
+                }
+
+                retStr.Append(lines[i]);
+            }
+            
+            return retStr.ToString();
+        }
+
+        private bool checkCopyright(int start)
+        {
+            if(copyright[0] == lines[start] &&
+                copyright[1] == lines[start+1] &&
+                copyright[2] == lines[start + 2])
+                return true;
+            
+            return false;
+        }
+
+        private void loadCopyright(int start)
+        {
+            if((copyright[0] != "" &&
+                copyright[1] != "" &&
+                copyright[2] != "") &&
+                (passCriteria(lines[start]) &&
+                 passCriteria(lines[start+1]) &&
+                 passCriteria(lines[start+2])))
+            {
+                copyright[0] = lines[start];
+                copyright[1] = lines[start+1];
+                copyright[2] = lines[start+2];
+            }
+        }
+
+        private bool copyrightLoaded()
+        {
+            if(copyright[0] != "" &&
+                copyright[1] != "" &&
+                copyright[2] != "")
+                return true;
+
+            return false;
+        }
+
+        private bool passCriteria(string line)
+        {
+            if(!(line == "-----") && !(line.Contains("Cue:") && line.Contains("Page:")))
+                return true;
+
+            return false;
+        }
+
+        private string convertArrToStr(string[] strArr)
+        {
+            StringBuilder outStr = new StringBuilder();
+
+            foreach (string str in strArr)
+                outStr.Append(str);
+            
+            return outStr.ToString();
+        }
+
+        private void writeToFile(string[] newLines)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            save.InitialDirectory = path+"_PP.txt";
+            
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(save.FileName, convertArrToStr(newLines));
+            }
+        }
+
+        private void impTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (impTextBox.Text.Length > 0)
+            {
+                expBtn.Enabled = true;
+            }
+            else
+            {
+                expBtn.Enabled = false;
+            } 
+        }
+    }
+}
